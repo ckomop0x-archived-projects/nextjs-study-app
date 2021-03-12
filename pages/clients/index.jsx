959429bfdd7b8cd4efc28177ceb3e97d874c9d34
Pavel Klochkov <paul.klochkov@gmail.com>
2021-03-12 17:34:39 +0100

    Dynamic clients list

--- pages/clients/index.jsx
+++ pages/clients/index.jsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
+  const clients = [
+    { id: "pavel", name: "Pavel Klochkov" },
+    { id: "tina", name: "Kristina Kuzmenko" },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
