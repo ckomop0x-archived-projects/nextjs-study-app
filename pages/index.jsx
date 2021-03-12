407360f315f0af980a06dd9cda8f2ee90d6d8a2d
Pavel Klochkov <paul.klochkov@gmail.com>
2021-03-12 17:23:20 +0100

    Add first navigation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,14 @@
-export default function HomePage () {
-    return <div><h1>Home Page</h1></div>
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <ul>
+        <li>
+          <Link href={"/portfolio"}>Portfolio</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
